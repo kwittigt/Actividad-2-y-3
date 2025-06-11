c00239435f5280b1a2b39d2671e6aabe34c5c9db
kwittigt <kwittig@utem.cl>
2025-06-11 08:43:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.set_page_config(page_title="Evaluación Integradora", layout="centered")
-st.title("Evaluación Integradora – Ciencia de Datos")
+st.set_page_config(page_title="Proyecto Final", layout="centered")
+st.title("Proyecto Final – Ciencia de Datos")
 st.markdown("""
-Bienvenido/a a la aplicación de análisis y flujo ETL para datos de ausentismo laboral.
+¡Bienvenido/a a la plataforma de análisis y proceso ETL para datos laborales!
 
 Utiliza el menú lateral para:
 - Realizar un **análisis exploratorio interactivo** (EDA).
-- Ejecutar un **comparativo técnico de arquitectura**.
+- Ejecutar una **comparación técnica de arquitecturas**.
 """)
